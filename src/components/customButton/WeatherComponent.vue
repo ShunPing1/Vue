6d7726c8466100cb050080f35114269ec769d45e
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-10 17:02:46 +0800

    update

--- src/components/customButton/WeatherComponent.vue
+++ src/components/customButton/WeatherComponent.vue
@@ -9,11 +9,19 @@ export default {
     
     methods: {
         showArray(){
-            console.log(this.weatherData);
+            console.log(this.weatherData.weatherElement);
         }
     },
 
 }
+/*
+ 備忘:
+ 天氣概況:this.weatherData.weatherElement[0].time[0].parameter.parameterName
+ 降雨機率:this.weatherData.weatherElement[1].time[0].parameter.parameterName
+ 最低溫度:this.weatherData.weatherElement[2].time[0].parameter.parameterName
+ 體感:this.weatherData.weatherElement[3].time[0].parameter.parameterName
+ 最高溫度:this.weatherData.weatherElement[4].time[0].parameter.parameterName
+*/
 </script>
 <template lang="">
     <div  class="border-[#000] border-[2px]" @click="showArray">
--- src/views/WeatherCard.vue
+++ src/views/WeatherCard.vue
@@ -14,33 +14,38 @@ export default {
             catchData: [],
             // place:['北部','中部','南部','東部','離島']
             place: [
-                {   
-                    id:1,
-                    name:'全台',
+                {
+                    id: 1,
+                    name: '全台',
                 },
-                {   
-                    id:2,
-                    name:'北部',
+                {
+                    id: 2,
+                    name: '北部',
                 },
-                {   
-                    id:3,
-                    name:'中部',
+                {
+                    id: 3,
+                    name: '中部',
                 },
-                {   
-                    id:4,
-                    name:'南部',
+                {
+                    id: 4,
+                    name: '南部',
                 },
-                {   
-                    id:5,
-                    name:'東部',
+                {
+                    id: 5,
+                    name: '東部',
                 },
-                {   
-                    id:6,
-                    name:'離島',
+                {
+                    id: 6,
+                    name: '離島',
                 }],
-            
-            placeID: 0,
-            
+
+            placeID: 1,
+            north: ['基隆市','臺北市','新北市','桃園市','新竹縣','新竹市','宜蘭縣'],
+            center: ['苗栗縣','臺中市','彰化縣','南投縣','雲林縣'],
+            south:['嘉義縣','嘉義市','高雄市','臺南市','屏東縣','澎湖縣'],
+            east:['花蓮縣','臺東縣'],
+            island:['金門縣','連江縣'],
+
         }
     },
     // 網頁載入時抓取API
@@ -53,13 +58,25 @@ export default {
                 this.catchData = data.records.location;
             });
     },
-    // computed: {
-    //     filterPlace(){
-    //         return if(){
-
-    //         }
-    //     }
-    // },
+    computed: {
+        filterPlace() {
+            return this.catchData.filter((item) => {
+                if (this.placeID === 1) {
+                    return true;
+                }else if (this.placeID === 2){
+                    return this.north.includes(item.locationName);
+                }else if (this.placeID === 3){
+                    return this.center.includes(item.locationName);
+                }else if (this.placeID === 4){
+                    return this.south.includes(item.locationName);
+                }else if (this.placeID === 5){
+                    return this.east.includes(item.locationName);
+                }else{
+                    return this.island.includes(item.locationName);
+                }
+            })
+        }
+    },
     methods: {
         showData() {
             console.log('weather data:', this.catchData[0]);
@@ -70,10 +87,10 @@ export default {
 <template lang="">
     <div class="flex gap-[5px]">
         <div v-for="item in place" :key="item.id" class="hover:bg-[#9bdff0]">
-            <WeatherSlot :place-btn="item" @click="place = item.id"></WeatherSlot>
+            <WeatherSlot :place-btn="item" @click="placeID = item.id"></WeatherSlot>
         </div>
     </div>
-    <div v-for="item in catchData" :key="item.id" >
+    <div v-for="item in filterPlace" :key="item.id" >
         <WeatherComponent :weather-data="item">
         </WeatherComponent>
     </div>
