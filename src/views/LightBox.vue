e0036379c5bc19916f1ac2b1b582be3b77a89041
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-21 06:54:50 +0800

    0121: lightbox update

--- src/views/LightBox.vue
+++ src/views/LightBox.vue
@@ -11,9 +11,10 @@ export default {
 </script>
 <template>
     <div>
-        <a class="example-image-link" href="../assets/image/cloudy.jpg" data-lightbox="example-1">
-            <img class="example-image" src="../assets/image/cloudy.jpg" alt="image-1" />
-        </a>
+        <a class="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-2.jpg" data-lightbox="example-2" data-title="Optional caption."><img class="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-2.jpg" alt="image-1"/></a>
+        <a class="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-2.jpg" data-lightbox="example-2" data-title="Optional caption." hidden><img class="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-2.jpg" alt="image-1"/></a>
+        <a class="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-2.jpg" data-lightbox="example-2" data-title="Optional caption." hidden><img class="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-2.jpg" alt="image-1"/></a>
+
 
     </div>
   </template>
