c6fc530ee0dedd1a85329597df6c5b1161b3424f
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-23 16:55:54 +0800

    0123: 漢堡條

--- src/views/AosPage.vue
+++ src/views/AosPage.vue
@@ -1,19 +1,43 @@
-<template lang="">
-    <div data-aos="fade-up" data-aos-duration="1000">
-    这是一个使用aos.js的动画效果
-    </div>  
-</template>
-<script>
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-export default {
+<template>
+    <div>
+      <div v-if="showA" class="a-object text-[#f00]">A物件</div>
+      <div class="content h-[2000px]">這是一個很長的內容...</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        showA: false
+      }
+    },
     mounted() {
-        AOS.init();
+      window.addEventListener('scroll', this.handleScroll)
+    },
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.handleScroll)
+    },
+    methods: {
+      handleScroll() {
+        const aObject = document.querySelector('.a-object')
+        const content = document.querySelector('.content')
+        const aObjectTop = aObject.offsetTop
+        const contentBottom = content.offsetTop + content.offsetHeight
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+  
+        if (scrollTop >= aObjectTop && scrollTop <= contentBottom) {
+          this.showA = true
+        } else {
+          this.showA = false
+        }
+      }
     }
-}
-
-</script>
-<style lang="">
-    
-</style>
+  }
+  </script>
+  
+  <style>
+  .a-object {
+    display: none;
+  }
+  </style>
