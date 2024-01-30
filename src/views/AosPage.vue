68ad1ed44fe71ace31f37c4321963776d21e7a6c
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-30 17:01:11 +0800

    0130: longer update

--- src/views/AosPage.vue
+++ src/views/AosPage.vue
@@ -1,33 +1,19 @@
 <template>
-  <div v-if="change" class="text-[150px]" ref="container" @click="changeIng">
-    change
+  <div>
+    <div ref="objectA" @click="scrollToB" class="w-[200px] h-[200px] bg-[#faa]">A</div>
+    <div class="longContent h-[3000px]"></div>
+    <div ref="objectB" class="w-[200px] h-[200px] bg-[#aaf]">B</div>
+    <div class="long h-[1000px]"></div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      change: true,
-    };
-  },
   methods: {
-    changeIng() {
-      this.change = !this.change;
-      console.log(this.change);
-    },
-    handleOutsideClick(event) {
-      if (!this.$refs.container.contains(event.target)) {
-        this.change = false;
-        console.log('!change',this.change);
-      }
-    },
-  },
-  mounted() {
-    document.addEventListener('click', this.handleOutsideClick);
-  },
-  // beforeDestroy() {
-  //   document.removeEventListener('click', this.handleOutsideClick);
-  // },
+    scrollToB() {
+      const bElement = this.$refs.objectB;
+      bElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 };
 </script>
